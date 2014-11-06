86b4e9e7f19224f4799f568d1728deeeac0ddd72
fionhan <1056856566@qq.com>
2014-11-06 12:23:45 +0800

    use config to control the program

--- UnityTest/Program.cs
+++ UnityTest/Program.cs
@@ -13,6 +13,13 @@ namespace UnityTest
     {
         static void Main(string[] args)
         {
+            if (ConfigurationManager.AppSettings["UseNewStructure"] != null &&
+                ConfigurationManager.AppSettings["UseNewStructure"].ToString() == "true")
+            {
+                Console.WriteLine("通过appSettings配置此处不执行其他语句直接返回");
+                return;
+            }
+            Console.WriteLine("appSettings中不配置值为true的UseNewStructure节点，下面根据Unity配置进行依赖注入");
             IUnityContainer container = new UnityContainer();
             //注入时要注入当前目标类以及其依赖的类-当不用配置指定注册时，将程序用这种方式写死
             //container.RegisterType<IMonitor, Monitor>().RegisterType<INotify, EmailNotify>();
